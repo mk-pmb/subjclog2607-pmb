d51a5d6a4de0427906318323c20c90b5bee14b29
mk-pmb <mk-pmb@users.noreply.github.com>
2026-07-06 03:53:16 +0200

    v0.1.0: This should work.

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+const EX = function makeLogger(topic) {
+  const cons = console;
+  const tag = topic + ':';
+  const log = cons.log.bind(cons, tag);
+  log.dbg = cons.debug.bind(cons, tag);
+  log.err = cons.error.bind(cons, tag);
+  log.warn = cons.warn.bind(cons, tag);
+  return log;
+};
+
+module.exports = EX;
